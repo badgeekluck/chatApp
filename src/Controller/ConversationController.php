d98d7be20b3f61ac74ea1703aa4196dc06d87357
mnm <mnm@harunbas-pc.local>
2020-09-28 15:47:43 +0300

    creating conversation, and get conversation added with queries

--- src/Controller/ConversationController.php
+++ src/Controller/ConversationController.php
@@ -2,18 +2,106 @@
 
 namespace App\Controller;
 
+use App\Entity\Conversation;
+use App\Entity\Participant;
+use App\Repository\ConversationRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/conversations", name="conversations.")
+ */
 class ConversationController extends AbstractController
 {
     /**
-     * @Route("/conversation", name="conversation")
+     * @var UserRepository
      */
-    public function index()
+    private $userRepository;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+    /**
+     * @var ConversationRepository
+     */
+    private $conversationRepository;
+
+    public function __construct(UserRepository $userRepository, EntityManagerInterface $entityManager, ConversationRepository $conversationRepository)
     {
-        return $this->render('conversation/index.html.twig', [
-            'controller_name' => 'ConversationController',
-        ]);
+        $this->userRepository = $userRepository;
+        $this->entityManager = $entityManager;
+        $this->conversationRepository = $conversationRepository;
     }
+
+    /**
+     * @Route("/", name="newConversations", methods={"POST"})
+     * @param Request $request
+     * @return JsonResponse
+     * @throws Exception
+     */
+    public function index(Request $request)
+    {
+        $otherUser = $request->get('otherUser', 0);
+        $otherUser = $this->userRepository->find($otherUser);
+
+        if (is_null($otherUser)) {
+            throw new Exception("The user was not found.");
+        }
+
+        if ($otherUser->getId() === $this->getUser()->getId()) {
+            throw new Exception("Can not create a conversation with yourself.");
+        }
+
+        $conversation = $this->conversationRepository->findConversationByParticipants($otherUser->getId(), $this->getUser()->getId());
+
+        if (count($conversation)) {
+            throw new Exception("The conversation already exists");
+        }
+
+        $conversation = new Conversation();
+
+        $participant = new Participant();
+        $participant->setUser($this->getUser());
+        $participant->setConversation($conversation);
+
+        $otherParticipant = new Participant();
+
+        $otherParticipant->setUser($otherUser);
+        $otherParticipant->setConversation($conversation);
+
+        $this->entityManager->beginTransaction();
+
+        try {
+            $this->entityManager->persist($conversation);
+            $this->entityManager->persist($participant);
+            $this->entityManager->persist($otherParticipant);
+
+            $this->entityManager->flush();
+            $this->entityManager->commit();
+        } catch (Exception $e) {
+            $this->entityManager->rollback();
+            throw $e;
+        }
+
+        return $this->json([
+            'conversationId' => $conversation->getId()
+        ], Response::HTTP_CREATED, [], []);
+    }
+
+    /**
+     * @Route("/", name="getConversations", methods={"GET"})
+     */
+    public function getConversations()
+    {
+        $conversations = $this->conversationRepository->findConversationsByUser($this->getUser()->getId());
+
+        return $this->json($conversations);
+    }
+
 }
--- src/Repository/ConversationRepository.php
+++ src/Repository/ConversationRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Conversation;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query\Expr\Join;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -19,32 +20,58 @@ class ConversationRepository extends ServiceEntityRepository
         parent::__construct($registry, Conversation::class);
     }
 
-    // /**
-    //  * @return Conversation[] Returns an array of Conversation objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @param int|null $otherUserId
+     * @param int $currentUserId
+     * @return int|mixed|string
+     */
+    public function findConversationByParticipants(?int $otherUserId, int $currentUserId)
     {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('c.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
+        $qb = $this->createQueryBuilder('c');
+        $qb
+            ->select($qb->expr()->count('p.conversation'))
+            ->innerJoin('c.participants', 'p')
+            ->where(
+                $qb->expr()->orX(
+                    $qb->expr()->eq('p.user', ':currentUserId'),
+                    $qb->expr()->eq('p.user', ':otherUserId')
+                )
+            )
+            ->groupBy('p.conversation')
+            ->having(
+                $qb->expr()->eq(
+                    $qb->expr()->count('p.conversation'),
+                    2
+                )
+            )
+            ->setParameters([
+                'currentUserId' => $currentUserId,
+                'otherUserId' => $otherUserId
+            ])
         ;
-    }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Conversation
-    {
-        return $this->createQueryBuilder('c')
-            ->andWhere('c.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $qb->getQuery()->getResult();
     }
-    */
+
+    /**
+     * @param int $userId
+     * @return int|mixed|string
+     */
+    public function findConversationsByUser(int $userId)
+    {
+        $qb = $this->createQueryBuilder('c');
+        $qb
+            ->select('otherUser.username', 'c.id as conversationId', 'lm.content', 'lm.createdAt')
+            ->innerJoin('c.participants', 'p', Join::WITH, $qb->expr()->neq('p.user', ':user'))
+            ->innerJoin('c.participants', 'me', Join::WITH, $qb->expr()->eq('me.user', ':user'))
+            ->leftJoin('c.lastMessage', 'lm')
+            ->innerJoin('me.user', 'meUser')
+            ->innerJoin('p.user', 'otherUser')
+            ->where('meUser.id = :user')
+            ->setParameter('user', $userId)
+            ->orderBy('lm.createdAt', 'DESC')
+        ;
+
+        return $qb->getQuery()->getResult();
+    }
 }
