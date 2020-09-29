45f9e97d225a4c50d1341e07835f296516b8659d
badgeekluck <harun.1996.187@gmail.com>
2020-09-29 15:40:06 +0300

    update event added, serializer added.

--- src/Repository/ParticipantRepository.php
+++ src/Repository/ParticipantRepository.php
@@ -19,32 +19,22 @@ class ParticipantRepository extends ServiceEntityRepository
         parent::__construct($registry, Participant::class);
     }
 
-    // /**
-    //  * @return Participant[] Returns an array of Participant objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    public function findParticipantByConversationIdAndUserId(int $conversationId, int $userId)
     {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
+        $qb = $this->createQueryBuilder('p');
+        $qb->
+        where(
+            $qb->expr()->andX(
+                $qb->expr()->eq('p.conversation', ':conversationID'),
+                $qb->expr()->neq('p.user', ':userId')
+            )
+        )
+            ->setParameters([
+                'conversationId' => $conversationId,
+                'userId' => $userId
+            ])
         ;
-    }
-    */
 
-    /*
-    public function findOneBySomeField($value): ?Participant
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
+        return $qb->getQuery()->getOneOrNullResult();
     }
-    */
 }
